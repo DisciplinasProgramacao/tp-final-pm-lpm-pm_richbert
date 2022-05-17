bb5f749b30db53b7d7daa862009431fc75560b88
Thiago Sougo <thiagosougo@gmail.com>
2022-05-16 21:57:33 -0300

    Cliente

--- Codigo/src/Cadastrado.java
+++ Codigo/src/Cadastrado.java
@@ -1,5 +1,7 @@
 package Codigo.src;
 
+import java.util.ArrayList;
+
 public class Cadastrado extends Cliente {
     public Cadastrado(String name, String userName, String password, ArrayList<Compra> compras){
         super(name, userName, password, compras);
--- Codigo/src/Empolgado.java
+++ Codigo/src/Empolgado.java
@@ -1,7 +1,9 @@
 package Codigo.src;
 
+import java.util.ArrayList;
+
 public class Empolgado extends Cliente{
-    public Empolgado(String name, String userName, String password){
-        super(name, userName, password);
+    public Empolgado(String name, String userName, String password, ArrayList<Compra> compras){
+        super(name, userName, password, compras);
     }
 }
