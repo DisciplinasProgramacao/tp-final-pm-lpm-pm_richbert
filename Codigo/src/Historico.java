5519c135e36ad678f57a098ec6573ccb11b2dcc5
Sara Iglesias <sara.iglesias0309@gmail.com>
2022-06-20 22:34:13 -0300

    Histórico com métodos corrigidos

--- Codigo/src/Historico.java
+++ Codigo/src/Historico.java
@@ -4,16 +4,43 @@ import java.util.ArrayList;
 
 public class Historico extends XulambGames{
     private ArrayList<Compra> compras;
- 
+    private double soma;
+
     @Override
     public double avgSellPrice(){
-        double media = compras.stream().mapToDouble(Compra::getPrice).average().getAsDouble();
+        double media = compras.stream().mapToDouble(Compra::getFinalPrice).average().getAsDouble();
         return media;
     }
 
     @Override
     public double monthlyAmountSold(int mes, int ano){
         double amountSoldByMonth = compras.stream()
-                                    .map(C -> C.getDate().getMonth() == mes)
+                                          .filter(c -> c.getDate().getYear() == ano && c.getDate().getMonthValue() == mes)
+                                          .mapToDouble(Compra::getFinalPrice)
+                                          .sum();
+
+        return amountSoldByMonth;
+    }
+
+    @Override
+    public String bestSoldGame(ArrayList<Jogo> jogos) {
+        Jogo bestSoldGame = jogos.stream()
+                                 .max((j1, j2) -> (
+                                     j1.vendido() > j2.vendido() ? 1 : -1
+                                 ))
+                                 .get();
+
+        return bestSoldGame.toString();
+    }
+
+    @Override
+    public String leastSoldGame(ArrayList<Jogo> jogos) {
+        Jogo bestSoldGame = jogos.stream()
+                                 .min((j1, j2) -> (
+                                     j1.vendido() > j2.vendido() ? 1 : -1
+                                 ))
+                                 .get();
+
+        return bestSoldGame.toString();
     }
 }
